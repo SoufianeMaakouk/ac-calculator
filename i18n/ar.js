f8a5faf67b26338b9df9d12d1f7a97575e7484a4
Soufiane Maakouk <soufianemaakouk@gmail.com>
2026-01-05 12:55:38 +0100

    Create ar.js

---
+++ i18n/ar.js
@@ -0,0 +1,25 @@
+window.TRANSLATIONS = {
+  appTitle: "مخطط تركيب التكييف",
+  addRoom: "إضافة غرفة",
+  calculate: "حساب المشروع",
+  projectName: "اسم المشروع",
+  saveProject: "حفظ المشروع",
+  exportPDF: "تصدير PDF",
+  logout: "تسجيل الخروج",
+
+  room: "غرفة",
+  area: "المساحة (م²)",
+  ceilingHeight: "ارتفاع السقف",
+  distance: "طول المواسير (م)",
+
+  bedroom: "غرفة نوم",
+  living: "غرفة معيشة",
+  kitchen: "مطبخ",
+
+  sunLow: "تعرض شمس ضعيف",
+  sunMedium: "تعرض شمس متوسط",
+  sunHigh: "تعرض شمس عالي",
+
+  systemRecommendation: "النظام المقترح",
+  totalBTU: "إجمالي BTU"
+};
