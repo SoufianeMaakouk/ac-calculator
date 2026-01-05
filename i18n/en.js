2a6a24d4ea962b543d1e4f74295e56dd4f5b0c5c
Soufiane Maakouk <soufianemaakouk@gmail.com>
2026-01-05 12:55:03 +0100

    Create en.js

---
+++ i18n/en.js
@@ -0,0 +1,19 @@
+auth.js
+Implement login and registration functions
+1 hour ago
+dashbaord.html
+Create dashbaord.html
+1 hour ago
+dashboard.js
+Create dashboard.js
+1 hour ago
+index.html
+Fix HTML syntax for meta and link tags
+4 days ago
+login.html
+Create login.html
+1 hour ago
+script.js
+Refactor language strings and project management
+4 days ago
+style.css
